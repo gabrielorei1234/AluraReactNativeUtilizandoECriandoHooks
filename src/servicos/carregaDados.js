d88f289eca19cd78fad5cb5fbd1b7352c4fe9f3f
gabriel.oliveira <gabriel.oliveira@pamafa.com.br>
2022-10-20 16:05:44 -0300

    first commit

---
+++ src/servicos/carregaDados.js
@@ -0,0 +1,10 @@
+import produtores from "../mocks/produtores";
+import topo from "../mocks/topo";
+
+export const CarregaProdutores = () => {
+    return produtores;
+}
+
+export const CarregaTopo = () => {
+    return topo;
+}
